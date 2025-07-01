8b9548c9cb2f81d8f0f553cef480f0d0553aa722
abc <clvlajrrhtlvek@gmail.com>
2025-07-01 22:05:11 +0900

    initial

---
+++ docq/src/main/java/api/docq/DocqApplication.java
@@ -0,0 +1,13 @@
+package api.docq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocqApplication.class, args);
+    }
+
+}
---
+++ docq/src/test/java/api/docq/DocqApplicationTests.java
@@ -0,0 +1,13 @@
+package api.docq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
